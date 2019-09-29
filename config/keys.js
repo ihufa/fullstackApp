f68c5466a9a533ae17ccefc4918082f7afd088de
ihufa <42546508+ihufa@users.noreply.github.com>
2019-09-29 16:41:51 +0200

    second try

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:admin123@mycluster-m9k0o.mongodb.net/test?retryWrites=true',
-    gmailLogin: {
-        user: 'planthooddk@gmail.com',
-        pass: 'Kerrig4n5000'
-    }
-}
